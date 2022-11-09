a98ea98d6011d2befe74523c18c5a354ef03ae6f
ianl <ianlee-tyl@gmail.com>
2022-11-08 23:16:09 -0500

    Updated nav bar

--- client/components/common/NavBar.vue
+++ client/components/common/NavBar.vue
@@ -1,18 +1,17 @@
 <!-- A basic navigation bar component -->
 <!-- Example of a component which is included on all pages (via App.vue) -->
 <!-- This navbar takes advantage of both flex and grid layouts for positioning elements; feel free to redesign as you see fit! -->
-
 <template>
   <nav>
     <div class="left">
-      <img src="../../public/logo.svg">
+      <img src="../../public/a.svg">
       <h1 class="title">
         Fritter
       </h1>
     </div>
     <div class="right">
       <router-link to="/">
-        Home
+        Feeds
       </router-link>
       <router-link
         v-if="$store.state.username"
@@ -41,8 +40,13 @@
 
 <style scoped>
 nav {
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
     padding: 1vw 2vw;
-    background-color: #ccc;
+
+    background: rgb(19,125,255);
+  background: linear-gradient(90deg, rgba(19,125,255,1) 0%, rgba(60,143,217,1) 9%, rgba(255,224,0,1) 100%);
+
+    box-shadow: 2px 2px 2px 2px rgba(8, 7, 7, 0.1);
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -51,7 +55,7 @@ nav {
 
 .title {
     font-size: 32px;
-    margin: 0 5px;
+    margin: 0 20px;
 }
 
 img {
@@ -61,6 +65,7 @@ img {
 .left {
 	display: flex;
 	align-items: center;
+  margin: 0 20px;
 }
 
 .right {
