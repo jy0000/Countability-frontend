6709de052946d6d786a93a97d1bb0d6cd195a332
ianl <ianlee-tyl@gmail.com>
2022-11-28 16:30:14 -0500

    Implemented utils for work session

--- server/workSession/util.ts
+++ server/workSession/util.ts
@@ -1,17 +1,14 @@
 import type {HydratedDocument} from 'mongoose';
 import moment from 'moment';
-import type {Session, PopulatedSession} from './model';
+import type {WorkSession, PopulatedWorkSession} from './model';
 
-// Update this if you add a property to the Session type!
-type SessionResponse = {
+type WorkSessionResponse = {
   _id: string;
-  author: string;
-  dateCreated: string;
-  content: string;
-  dateModified: string;
-  sessionType: string;
-  sourceLink: string;
-  emoji: string;
+  startDate: string;
+  endDate: string;
+  sessionOwner: string;
+  numChecks: string;
+  checks: string;
 };
 
 /**
@@ -29,27 +26,26 @@ const formatDate = (date: Date): string => moment(date).format('MMMM Do YYYY, h:
  * @param {HydratedDocument<Session>} session - A session
  * @returns {SessionResponse} - The session object formatted for the frontend
  */
-const constructWorkSessionResponse = (session: HydratedDocument<Session>): SessionResponse => {
-  const sessionCopy: PopulatedSession = {
+const constructWorkSessionResponse = (session: HydratedDocument<WorkSession>): WorkSessionResponse => {
+  const sessionCopy: PopulatedWorkSession = {
     ...session.toObject({
       versionKey: false // Cosmetics; prevents returning of __v property
     })
   };
-  const {username} = sessionCopy.authorId;
-  delete sessionCopy.authorId;
+  // Make copies to avoid mutation
+  const sessionOwnerUsername = sessionCopy.sessionOwner.username;
+  delete sessionCopy.sessionOwner;
   return {
     ...sessionCopy,
     _id: sessionCopy._id.toString(),
-    author: username,
-    dateCreated: formatDate(session.dateCreated),
-    dateModified: formatDate(session.dateModified),
-    sessionType: sessionCopy.sessionType,
-    // Defaults to empty string if the session type does not have this property.
-    sourceLink: sessionCopy.sessionType === 'News' ? sessionCopy.sourceLink : '',
-    emoji: sessionCopy.sessionType === 'Fibe' ? sessionCopy.emoji : ''
+    startDate: formatDate(session.startDate),
+    endDate: formatDate(session.endDate),
+    sessionOwner: sessionOwnerUsername,
+    numChecks: sessionCopy.numChecks.toString(),
+    checks: sessionCopy.checks.toString()
   };
 };
 
 export {
-  constructSessionResponse
+  constructWorkSessionResponse
 };
